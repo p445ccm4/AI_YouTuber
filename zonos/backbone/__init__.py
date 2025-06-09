b95b635dcf7a516f1ce5b61c28c92209a0170ddf
p445ccm4 <p445ccm4@yahoo.com.hk>
2025-06-09 13:39:25 +0800

    deleted Zonos

--- zonos/backbone/__init__.py
+++
@@ -1,9 +0,0 @@
-BACKBONES = {}
-
-from ._mamba_ssm import MambaSSMZonosBackbone
-
-BACKBONES["mamba_ssm"] = MambaSSMZonosBackbone
-
-from ._torch import TorchZonosBackbone
-
-BACKBONES["torch"] = TorchZonosBackbone
